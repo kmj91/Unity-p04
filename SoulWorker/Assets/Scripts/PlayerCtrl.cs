e479f6fe9326b915f495c43db107b1d833f47268
kmj91 <lethita0302@gmail.com>
2021-06-15 11:05:50 +0900

    카메라 회전 구현
    x축 회전 제한

--- SoulWorker/Assets/Scripts/PlayerCtrl.cs
+++ SoulWorker/Assets/Scripts/PlayerCtrl.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+
 using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
+using System;
 
 public class PlayerCtrl : MonoBehaviour
 {
     public float moveSpeed = 5.0f;     // 이동속도
+    public Transform aimTransform;
     public Animator hairAnime;
     public Animator faceAnime;
     public Animator bodyAnime;
@@ -15,6 +19,7 @@ public class PlayerCtrl : MonoBehaviour
     public Animator footAnime;
 
     private Transform playerTransform;
+    private float cameraSpeed = 100.0f;
     private float h = 0.0f;
     private float v = 0.0f;
 
@@ -27,10 +32,12 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 이동 방향키
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
-        Vector3 vecDir = (Vector3.forward * v) + (Vector3.right * h);
+        Vector3 vecDir = (aimTransform.forward * v) + (aimTransform.right * h);
+        vecDir.y = 0.0f;
         playerTransform.Translate(vecDir.normalized * moveSpeed * Time.deltaTime, Space.World);
 
         hairAnime.SetFloat("Speed", v);
@@ -39,5 +46,31 @@ public class PlayerCtrl : MonoBehaviour
         pantsAnime.SetFloat("Speed", v);
         handsAnime.SetFloat("Speed", v);
         footAnime.SetFloat("Speed", v);
+
+        AimRotation();
+    }
+
+    // 에임 회전
+    void AimRotation()
+    {
+        var yRotation = Input.GetAxis("Mouse X");
+        var xRotation = Input.GetAxis("Mouse Y");
+
+        // X축 회전 제한
+        var rotation = aimTransform.rotation * Quaternion.Euler(new Vector3(-xRotation, 0, 0));
+        var xAngle = Mathf.Round(rotation.eulerAngles.x);
+        if (180.0f <= xAngle && xAngle < 310.0f)
+        {
+            xAngle = 310.0f;
+        }
+        else if (180.0f > xAngle && xAngle > 50.0f)
+        {
+            xAngle = 50.0f;
+        }
+
+        aimTransform.rotation = Quaternion.Euler(new Vector3(xAngle, aimTransform.rotation.eulerAngles.y, aimTransform.rotation.eulerAngles.x));
+
+        // Y축 자전
+        aimTransform.RotateAround(playerTransform.position, Vector3.up, yRotation * cameraSpeed * Time.deltaTime);
     }
 }
--- SoulWorker/Assets/Scripts/SmoothFollow.cs
+++ SoulWorker/Assets/Scripts/SmoothFollow.cs
@@ -6,32 +6,28 @@ public class SmoothFollow : MonoBehaviour
 {
     public Transform target;
     public float distance = 10.0f;
-    public float height = 5.0f;
     private float rotationDamping = 3.0f;
-    private float heightDamping = 2.0f;
 
     void LateUpdate()
     {
         if (!target)
             return;
 
-        var wantedRotationAngle = target.eulerAngles.y;
-        var wantedHeight = target.position.y + height;
 
-        var currentRotationAngle = transform.eulerAngles.y;
-        var currentHeight = transform.position.y;
+        var wantedRotationAngleY = target.eulerAngles.y;
+        var wantedRotationAngleX = target.eulerAngles.x;
+
+        var currentRotationAngleY = transform.eulerAngles.y;
+        var currentRotationAngleX = transform.eulerAngles.x;
 
 
-        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentRotationAngleY = Mathf.LerpAngle(currentRotationAngleY, wantedRotationAngleY, rotationDamping * Time.deltaTime);
+        currentRotationAngleX = Mathf.LerpAngle(currentRotationAngleX, wantedRotationAngleX, rotationDamping * Time.deltaTime);
 
-        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
-
-        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+        var currentRotation = Quaternion.Euler(currentRotationAngleX, currentRotationAngleY, 0);
 
         transform.position = target.position;
         transform.position -= currentRotation * Vector3.forward * distance;
-
-        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 
         transform.LookAt(target);
     }
