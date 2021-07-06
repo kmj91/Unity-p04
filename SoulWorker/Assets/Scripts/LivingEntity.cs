f2ff8a72909df21de0dbf4227104f381f5792526
kmj91 <lethita0302@gmail.com>
2021-07-06 23:54:24 +0900

    LivingEntity 추가

---
+++ SoulWorker/Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MyStruct;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public float startingHealth = 100.0f;
+    public float health { get; protected set; }
+    public bool dead { get; protected set; }
+    public event Action OnDeath;    // 사망할 때 호출할 콜백 함수
+
+    // 0.1초 내에 공격이 여러번 들어오면 무시
+    private const float minTimeBetDamaged = 0.1f;
+    private float lastDamagedTime;
+
+    protected bool IsInvulnerable
+    {
+        get 
+        {
+            if (Time.time >= lastDamagedTime + minTimeBetDamaged) return false;
+
+            return true;
+        }
+    }
+
+
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = startingHealth;
+    }
+
+    public virtual bool ApplyDamage(DamageMessage damageMessage)
+    {
+        if (IsInvulnerable || damageMessage.damager == gameObject || dead) return false;
+
+        lastDamagedTime = Time.time;
+        health -= damageMessage.amount;
+
+        if (health <= 0) Die();
+
+        return true;
+    }
+
+    public virtual void RestoreHealth(float newHealth)
+    {
+        if (dead) return;
+
+        health += newHealth;
+    }
+
+    public virtual void  Die()
+    {
+        if (OnDeath != null) OnDeath();
+
+        dead = true;
+    }
+}
