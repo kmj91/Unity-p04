4c68489397904ba48215853fca34c57069a65e9b
kmj91 <lethita0302@gmail.com>
2021-07-12 23:56:47 +0900

    UI 매니저 추가

---
+++ SoulWorker/Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null) instance = FindObjectOfType<UIManager>();
+
+            return instance;
+        }
+    }
+
+    [SerializeField] private Image hpBar;
+    [SerializeField] private Text hpText;
+}
