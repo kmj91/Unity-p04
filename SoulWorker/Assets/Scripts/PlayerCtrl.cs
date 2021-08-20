07fd31b49d4b463c6c1bea64d8635513debe8c3c
kmj91 <lethita0302@gmail.com>
2021-08-21 00:15:07 +0900

    사용하지 않는 함수 삭제

--- SoulWorker/Assets/Scripts/PlayerCtrl.cs
+++ SoulWorker/Assets/Scripts/PlayerCtrl.cs
@@ -289,43 +289,6 @@ public partial class PlayerCtrl : MonoBehaviour
         return changeState[(int)left, (int)right];
     }
 
-    // 이동
-    private void Move()
-    {
-        // 착지
-        if (state == HaruState.Land)
-            return;
-
-        // 방향키 입력이 없음
-        if (moveInput == Vector2.zero && !jump)
-        {
-            characterController.Move(Vector3.down);
-            return;
-        }
-
-        float smoothTime = characterController.isGrounded ? speedSmoothTime : speedSmoothTime / airControlPercent;
-        targetSpeed = Mathf.SmoothDamp(currentSpeed, moveSpeed, ref speedSmoothVelocity, smoothTime);
-        
-        Vector3 moveDir = (cameraTransform.forward * moveInput.y) + (cameraTransform.right * moveInput.x);
-        moveDir.y = 0.0f;
-        moveDir = moveDir.normalized;
-
-        currentVelocityY += Time.deltaTime * Physics.gravity.y * 3.0f;
-        Vector3 velocity = moveDir * targetSpeed + Vector3.up * currentVelocityY;
-        characterController.Move(velocity * Time.deltaTime);
-
-        // 현제 캐릭터가 바라보는 곳과 이동 방향이 같지 않으면 이동 방향값 셋팅
-        // 단 캐릭터 조작을 하지 않아서 가만히 있으면 X
-        if (modelTransform.forward != moveDir && Vector3.zero != moveDir)
-            turnDir = moveDir;
-
-        // 땅에 닿으면 초기화
-        if (characterController.isGrounded)
-        {
-            currentVelocityY = 0.0f;
-        }
-    }
-
     private void PlayerRotation()
     {
         // 방향이 같지 않음
