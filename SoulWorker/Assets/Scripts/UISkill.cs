5e00155ef6cb43309b310b0227a96fc05accf368
kmj91 <lethita0302@gmail.com>
2021-10-24 23:40:21 +0900

    스킬 버튼 멤버 변수
    레벨 업, 다운 등등

--- SoulWorker/Assets/Scripts/UISkill.cs
+++ SoulWorker/Assets/Scripts/UISkill.cs
@@ -3,13 +3,22 @@ using UnityEngine.UI;
 
 public class UISkill : MonoBehaviour
 {
-    [SerializeField] private Text[] m_skillLevel;     // 스킬 레벨 텍스트
+    [SerializeField] private Text[] m_skillLevel;               // 스킬 레벨 텍스트
+    [SerializeField] private Sprite[] m_skillButtonSprite;      // 스킬 버튼 스프라이트
+    [SerializeField] private Image[] m_skillLevelUpButton;      // 스킬 레벨 업 버튼 이미지
+    [SerializeField] private Image[] m_skillLevelDownButton;    // 스킬 레벨 다운 버튼 이미지
 
 
     // 창 닫기
     public void OnClickCloseSkillinfo()
     {
         gameObject.SetActive(false);
+    }
+
+    // 퍼스트 블레이드 스킬 레벨 업
+    public void OnClickFirstBladeLavelUp()
+    {
+        
     }
 
     // 스킬 정보 창
