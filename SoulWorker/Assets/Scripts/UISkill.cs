69cb568d938abed8ee058c8e7fbe61459824588f
kmj91 <lethita0302@gmail.com>
2021-10-27 23:51:22 +0900

    스킬 포인트 감소

--- SoulWorker/Assets/Scripts/UISkill.cs
+++ SoulWorker/Assets/Scripts/UISkill.cs
@@ -11,6 +11,7 @@ public class UISkill : MonoBehaviour
     [SerializeField] private Sprite[] m_skillButtonSprite;      // 스킬 버튼 스프라이트
     [SerializeField] private Image[] m_skillLevelUpButton;      // 스킬 레벨 업 버튼 이미지
     [SerializeField] private Image[] m_skillLevelDownButton;    // 스킬 레벨 다운 버튼 이미지
+    [SerializeField] private Text m_skillPoint;                 // 스킬 포인트 텍스트
 
 
     // 플레이어 정보
@@ -28,6 +29,11 @@ public class UISkill : MonoBehaviour
     // 퍼스트 블레이드 스킬 레벨 업
     public void OnClickFirstBladeLavelUp()
     {
+        int sp = m_haruinfo.GetSkillPoint();
+        // sp가 충분한지 검사
+        if (sp < 1)
+            return;
+
         int level = m_haruinfo.GetSkillLevel(HaruSkill.FirstBlade);
         // 최대 레벨
         if (level + 1 > 5)
@@ -37,6 +43,9 @@ public class UISkill : MonoBehaviour
         m_skillLevel[(int)HaruSkill.FirstBlade].text = ++level + "/5";
         // 증가한 레벨 플레이어 정보로
         m_haruinfo.SetSkillLevel(HaruSkill.FirstBlade, level);
+        // sp 감소
+        m_haruinfo.SetSkillPoint(--sp);
+        m_skillPoint.text = sp + " / " + m_haruinfo.GetMaxSkillPoint();
     }
 
     // 스킬 정보 창
