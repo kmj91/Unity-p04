4e6ee18bb8b5f9169c3e4b53ca8326df26b49d1b
kmj91 <lethita0302@gmail.com>
2021-11-28 23:59:21 +0900

    Scriptable Object 사용
    데이터를 다른 방식으로.. 시험...

---
+++ SoulWorker/Assets/Scripts/SkillDataScriptableObject.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillData", menuName = "ScriptableObjects/SkillDataScriptableObject", order = 1)]
+public class SkillDataScriptableObject : ScriptableObject
+{
+    [SerializeField] private int hp;
+    public int Hp { get { return hp; } }
+}
