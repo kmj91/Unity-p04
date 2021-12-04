74e1c3c942ee3aaea3c57ec931711311cdf5a04b
kmj91 <lethita0302@gmail.com>
2021-12-04 23:58:53 +0900

    다차원 배열 테스트

--- SoulWorker/Assets/Scripts/SkillDataScriptableObject.cs
+++ SoulWorker/Assets/Scripts/SkillDataScriptableObject.cs
@@ -6,4 +6,24 @@ public class SkillDataScriptableObject : ScriptableObject
 {
     [SerializeField] private int hp;
     public int Hp { get { return hp; } }
+
+
+    [System.Serializable]
+    public class SkillType
+    {
+        public class SkillLevel
+        {
+            public class SkillHitCount
+            {
+
+            }
+
+            public SkillHitCount[] HitCount;
+            public SkillHitCount this[int index] { get { return HitCount[index]; } }
+        }
+
+        public SkillLevel[] level;
+        public SkillLevel this[int index] { get { return level[index]; } }
+    }
+    [SerializeField] private SkillType[] m_skillDamage;    // 스킬 데미지
 }
